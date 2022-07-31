a70c32fc5d046b63a36a2d436463b001c81b547b
bobur <uzbek.4103@gmail.com>
2022-07-31 16:15:22 +0500

    Task #4. Loop Keywords v1.0

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-
+        boolean checkToLess = true;
+        for (int i = 0; i < numberToGoUntil; i++) {
+            if (numberToGoUntil < toBreakWith && checkToLess) {
+                System.out.println("iterating till the end");
+                numberToGoUntil++;
+                checkToLess = false;
+            }
+            if (i >= toBreakWith) {
+                System.out.println(i);
+                break;
+            } else if (i > 0){
+                System.out.println(i);
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -1,7 +1,30 @@
 package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
-    public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
-
+    public void printSumOfTwoRanges(int numberToSkip, int lastInRow) { // 5  10 ---- 15   40
+        int sum = numberToSkip;
+        int cnt = 0;
+        int iter = lastInRow < 0 ? lastInRow * -1 : lastInRow;
+        for (int i = 0; i < iter; i++) {
+            if (numberToSkip > lastInRow) {
+                System.out.println("number to skip is bugger then the last");
+                break;
+            } else if (lastInRow < 0) {
+                System.out.println("last number in row is negative");
+                break;
+            }
+            if (i % numberToSkip == 0) {
+                sum += numberToSkip;
+                continue;
+            }
+            cnt += i;
+        }
+        if (!(lastInRow < 0 || lastInRow < numberToSkip) && numberToSkip != lastInRow) {
+            System.out.println("skipped sum is " + sum); // 15
+            System.out.println("counted sum is " + cnt); // 40
+        } else if (numberToSkip == lastInRow) {
+            System.out.println("skipped sum is 55");
+            System.out.println("counted sum is 0");
+        }
     }
 }
